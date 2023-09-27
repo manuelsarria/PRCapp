bfbd68f6df8995381cbc402e000a79467f651bc2
Manuel Sarrria <victor.sarria@utp.ac.pa>
2023-09-27 00:31:02 -0500

    update api to v1.2.0

--- app.js
+++ app.js
@@ -11,9 +11,10 @@ app.post('/add', (req, res) => {
   const deliverydate = new Date().toISOString().slice(0, 10); // Obtiene la fecha actual en formato 'YYYY-MM-DD'
   const shipmentdate = null; // No establecemos el shipmentdate en este momento
 
-  const sql = 'INSERT INTO tracking (trackingnumber, deliverydate, shipmentdate) VALUES (?, ?, ?)';
+  const sql =
+    'INSERT INTO tracking (trackingnumber, deliverydate, shipmentdate, type) VALUES (?, ?, ?,?)';
 
-  db.query(sql, [trackingnumber, deliverydate, shipmentdate], (err, result) => {
+  db.query(sql, [trackingnumber, deliverydate, shipmentdate, type], (err, result) => {
     if (err) {
       console.error('Error al agregar el trackingnumber:', err);
       res.status(500).json({ message: 'Error al agregar el trackingnumber' });
@@ -30,18 +31,19 @@ app.post('/add-all', (req, res) => {
   const shipmentdate = null; // No establecemos el shipmentdate en este momento
 
   // Mapeamos los trackingnumbers para crear un array de valores para cada tracking number
-  const values = trackingnumbers.map((trackingnumber) => [
+  const values = trackingnumbers.map(({ trackingnumber, type }) => [
     trackingnumber,
     deliverydate,
     shipmentdate,
+    type,
   ]);
 
-  const sql = 'INSERT INTO tracking (trackingnumber, deliverydate, shipmentdate) VALUES ?';
+  const sql = 'INSERT INTO tracking (trackingnumber, deliverydate, shipmentdate, type) VALUES ?';
 
   db.query(sql, [values], (err, result) => {
     if (err) {
       console.error('Error al agregar los trackingnumbers:', err);
-      res.status(500).json({ message: 'Error al agregar los trackingnumbers' });
+      res.status(500).json({ message: `Error al agregar los trackingnumbers ${err}` });
     } else {
       res.status(201).json({ message: 'Los trackingnumbers se han agregado correctamente' });
     }
@@ -66,11 +68,13 @@ app.get('/tracking/:trackingnumber', (req, res) => {
           message: 'El paquete ha sido embarcado y entregado',
           deliverydate: packageInfo.deliverydate,
           shipmentdate: packageInfo.shipmentdate,
+          type: packageInfo.type,
         });
       } else if (packageInfo.deliverydate) {
         res.status(200).json({
           message: 'El paquete ha sido entregado en Bodega',
           deliverydate: packageInfo.deliverydate,
+          type: packageInfo.type,
         });
       } else {
         res.status(200).json({ message: 'El paquete no ha sido entregado' });
@@ -80,13 +84,13 @@ app.get('/tracking/:trackingnumber', (req, res) => {
 });
 
 // Método para obtener los trackingnumbers con deliverydate pero sin shipmentdate
-function getTrackingNumbersWithoutShipmentDate(callback) {
+function getTrackingNumbersWithoutShipmentDate(type, callback) {
   const sql =
-    'SELECT trackingnumber FROM tracking WHERE deliverydate IS NOT NULL AND shipmentdate IS NULL';
+    'SELECT trackingnumber FROM tracking WHERE deliverydate IS NOT NULL AND shipmentdate IS NULL AND type = ?';
 
-  db.query(sql, (err, result) => {
+  db.query(sql, [type], (err, result) => {
     if (err) {
-      console.error('Error al obtener los trackingnumbers sin shipmentdate:', err);
+      console.error(`Error al obtener los trackingnumbers sin shipmentdate para ${type}:`, err);
       callback([]);
     } else {
       const trackingNumbersWithoutShipmentDate = result.map((row) => row.trackingnumber);
@@ -98,6 +102,46 @@ function getTrackingNumbersWithoutShipmentDate(callback) {
 // Método para actualizar el shipmentdate en aquellos tracking numbers sin shipmentdate
 app.put('/update/shipmentdate', (req, res) => {
   getTrackingNumbersWithoutShipmentDate((trackingNumbersWithoutShipmentDate) => {
+    if (trackingNumbersWithoutShipmentDate.length === 0) {
+      res.status(200).json({ message: 'No hay tracking numbers sin shipmentdate para actualizar' });
+    } else {
+      const sql = 'UPDATE tracking SET shipmentdate = CURRENT_DATE() WHERE trackingnumber IN (?)';
+
+      db.query(sql, [trackingNumbersWithoutShipmentDate], (err, result) => {
+        if (err) {
+          console.error('Error al actualizar el shipmentdate:', err);
+          res.status(500).json({ message: 'Error al actualizar el shipmentdate' });
+        } else {
+          res.status(200).json({ message: 'El shipmentdate se ha actualizado correctamente' });
+        }
+      });
+    }
+  });
+});
+
+// Actualizar shipmentdate para AIR
+app.put('/update/shipmentdate/air', (req, res) => {
+  getTrackingNumbersWithoutShipmentDate('AIR', (trackingNumbersWithoutShipmentDate) => {
+    if (trackingNumbersWithoutShipmentDate.length === 0) {
+      res.status(200).json({ message: 'No hay tracking numbers sin shipmentdate para actualizar' });
+    } else {
+      const sql = 'UPDATE tracking SET shipmentdate = CURRENT_DATE() WHERE trackingnumber IN (?)';
+
+      db.query(sql, [trackingNumbersWithoutShipmentDate], (err, result) => {
+        if (err) {
+          console.error('Error al actualizar el shipmentdate:', err);
+          res.status(500).json({ message: 'Error al actualizar el shipmentdate' });
+        } else {
+          res.status(200).json({ message: 'El shipmentdate se ha actualizado correctamente' });
+        }
+      });
+    }
+  });
+});
+
+// Actualizar shipmentdate para MAR
+app.put('/update/shipmentdate/mar', (req, res) => {
+  getTrackingNumbersWithoutShipmentDate('MAR', (trackingNumbersWithoutShipmentDate) => {
     if (trackingNumbersWithoutShipmentDate.length === 0) {
       res.status(200).json({ message: 'No hay tracking numbers sin shipmentdate para actualizar' });
     } else {
